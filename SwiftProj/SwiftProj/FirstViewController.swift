c2b368f685488f91b781d17ca4c86e50e6c3ceee
jimple <jimple_chen@qq.com>
2015-08-17 10:47:32 +0800

    create a swift project.

---
+++ SwiftProj/SwiftProj/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftProj
+//
+//  Created by jimple on 15/8/17.
+//  Copyright (c) 2015年 JimpleChen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftProj/SwiftProj/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftProj
+//
+//  Created by jimple on 15/8/17.
+//  Copyright (c) 2015年 JimpleChen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
